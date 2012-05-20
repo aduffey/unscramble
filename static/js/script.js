a310f5883c1a86e14e0b2e1250a2d23377e4cd22
Andrew Duffey <ajduff574@gmail.com>
2012-05-20 15:35:10 -0400

    Add special handling for "Qu" cell in web form.
    When a user types "q" or "Q", it will automatically be transformed
    into "Qu".

--- static/js/script.js
+++ static/js/script.js
@@ -4,8 +4,14 @@ function handleKey(input, event, nextInputName) {
     event.preventDefault()  // We'll handle the input ourselves
     var key = event.keyCode || event.which;
     var keyChar = event.key || String.fromCharCode(key);
-    if (VALID_CHARS.lastIndexOf(keyChar.toUpperCase()) >= 0) {
-        input.value = keyChar.toUpperCase()
+    keyChar = keyChar.toUpperCase()
+    if (VALID_CHARS.lastIndexOf(keyChar) >= 0) {
+        // Special case: 'Q' really means 'Qu'. There is no 'Q' cell.
+        if (keyChar === 'Q') {
+            input.value = 'Qu'
+        } else {
+            input.value = keyChar
+        }
         if (nextInputName != '') {
             document.getElementsByName(nextInputName)[0].focus()
         }
@@ -17,6 +23,11 @@ function handleSubmit() {
     var boardString = ''
     for (var i = 0; i < 16; i++) {  // Only the first 16 elements
         c = form.elements[i].value.toLowerCase()
+        // Handle a 'Qu' cell. This is represented simply as 'q' in a board
+        // string.
+        if (c === 'qu') {
+            c = 'q'
+        }
         boardString += c
     }
     window.location.href = '/' + boardString
